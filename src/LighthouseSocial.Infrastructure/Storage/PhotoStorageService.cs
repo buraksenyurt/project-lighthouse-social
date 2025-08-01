fc5cc752171caf88f36400b94414a501e79aef6c
buraksenyurt <burakselimsenyurt@gmail.com>
2025-08-01 12:07:51 +0300

    Started to write photo sorage component.

---
+++ src/LighthouseSocial.Infrastructure/Storage/PhotoStorageService.cs
@@ -0,0 +1,52 @@
+﻿using LighthouseSocial.Domain.Interfaces;
+using Microsoft.Extensions.Options;
+using Minio;
+using Minio.DataModel.Args;
+
+namespace LighthouseSocial.Infrastructure.Storage
+{
+    public class PhotoStorageService
+        : IPhotoStorageService
+    {
+        private readonly IMinioClient _minioClient;
+        private readonly string _bucket;
+        public PhotoStorageService(IOptions<MinioSettings> options)
+        {
+            var settings = options.Value;
+            _bucket = settings.BucketName;
+            _minioClient = new MinioClient()
+                .WithEndpoint(settings.Endpoint)
+                .WithCredentials(settings.AccessKey, settings.SecretKey)
+                .WithSSL(settings.UseSSL)
+                .Build();
+        }
+        public Task DeleteAsync(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> GetAsync(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> SaveAsync(Stream content, string fileName)
+        {
+            bool found = await _minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket(_bucket));
+            if(!found)
+            {
+                await _minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket(_bucket));
+            }
+
+            await _minioClient.PutObjectAsync(
+                new PutObjectArgs()
+                    .WithBucket(_bucket)
+                    .WithObject(fileName)
+                    .WithStreamData(content)
+                    .WithObjectSize(content.Length)
+                    .WithContentType("application/octet-stream"));
+
+            return $"{_bucket}/{fileName}";
+        }
+    }
+}
