c92a449f29492d846553f6ffdd6b0f7b07961697
buraksenyurt <burakselimsenyurt@gmail.com>
2025-08-09 20:49:00 +0300

    Reorganized request models

--- src/LighthouseSocial.Application/Features/RequestModels.cs
+++ src/LighthouseSocial.Application/Features/RequestModels.cs
@@ -3,14 +3,22 @@
 namespace LighthouseSocial.Application.Features.Models
 {
     internal record CreateLighthouseRequest(LighthouseDto Lighthouse);
-    internal record GetLighthouseByIdRequest(Guid Id);
-    internal record DeleteLighthouseRequest(Guid id);
+    internal record DeleteLighthouseRequest(Guid Id);
     internal record GetAllLighthouseRequest();
+    internal record GetLighthouseByIdRequest(Guid Id);
 }
 
 namespace LighthouseSocial.Application.Features.Photo.Models
 {
+    internal record DeletePhotoRequest(Guid Id);
     internal record UploadPhotoRequest(PhotoDto Photo, Stream Content);
+}
+
+namespace LighthouseSocial.Application.Features.Comment.Models
+{
+    internal record AddCommentRequest(CommentDto Comment);
+    internal record DeleteCommentRequest(Guid CommentId);
+    internal record GetCommentByPhotoRequest(Guid PhotoId);
 }
 
 namespace LighthouseSocial.Application.Features.Common
