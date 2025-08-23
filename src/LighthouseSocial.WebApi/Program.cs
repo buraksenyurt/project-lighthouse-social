8e97bb82005eb7db1e347b28c116519cd1c58a1b
buraksenyurt <burakselimsenyurt@gmail.com>
2025-08-23 15:13:42 +0300

    Added logging support

--- src/LighthouseSocial.WebApi/Program.cs
+++ src/LighthouseSocial.WebApi/Program.cs
@@ -6,7 +6,8 @@ using LighthouseSocial.Infrastructure.Auditors;
 using LighthouseSocial.Infrastructure.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 builder.Services.AddControllers();
 builder.Services.AddApplication();
@@ -22,18 +23,11 @@ builder.Services.AddData();
 builder.Services.Configure<MinioSettings>(builder.Configuration.GetSection("Minio"));
 builder.Services.AddHttpClient<ICommentAuditor, ExternalCommentAuditor>();
 
-// builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.MapOpenApi();
-//}
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
