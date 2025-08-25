5fc1db41c43f158c149fd524437dc82eefecac4f
buraksenyurt <burakselimsenyurt@gmail.com>
2025-08-25 16:19:09 +0300

    Bounded to user service

---
+++ src/LighthouseSocial.Application/Contracts/ExternalServices/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using LighthouseSocial.Application.Common;
+using LighthouseSocial.Application.Dtos;
+
+namespace LighthouseSocial.Application.Contracts.ExternalServices;
+
+public interface IUserService
+{
+    Task<Result<Guid>> CreateUserAsync(UserDto userDto);
+}
---
+++ src/LighthouseSocial.Application/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using LighthouseSocial.Application.Common;
+using LighthouseSocial.Application.Common.Pipeline;
+using LighthouseSocial.Application.Contracts.ExternalServices;
+using LighthouseSocial.Application.Dtos;
+using LighthouseSocial.Application.Features.User;
+
+namespace LighthouseSocial.Application.Services;
+
+public class UserService(PipelineDispatcher pipelineDispatcher)
+    : IUserService
+{
+    public Task<Result<Guid>> CreateUserAsync(UserDto userDto)
+    {
+        var request = new CreateUserRequest(userDto);
+        return pipelineDispatcher.SendAsync<CreateUserRequest, Result<Guid>>(request);
+    }
+}
