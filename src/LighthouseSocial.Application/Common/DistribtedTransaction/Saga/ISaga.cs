42d9bb62eb48df0080431e6af388054a572ebd2d
buraksenyurt <burakselimsenyurt@gmail.com>
2025-09-09 12:56:27 +0300

    Added contract for SAGA

---
+++ src/LighthouseSocial.Application/Common/DistribtedTransaction/Saga/ISaga.cs
@@ -0,0 +1,12 @@
+﻿namespace LighthouseSocial.Application.Common.DistribtedTransaction.Saga;
+
+public interface ISaga<TRequest, TResponse>
+{
+    Task<TResponse> ExecuteAsync(TRequest request, CancellationToken cancellationToken = default);
+}
+
+public interface ISagaStep<TStepData>
+{
+    Task<Result<TStepData>> ExecuteAsync(TStepData data, CancellationToken cancellationToken = default);
+    Task CompensateAsync(TStepData data, CancellationToken cancellationToken = default);
+}
