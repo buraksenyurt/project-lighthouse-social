f011785dbfbb070d64665bcc470a28ebabfe3906
buraksenyurt <burakselimsenyurt@gmail.com>
2025-09-13 12:09:30 +0300

    Seperated photo and upload controllers

--- src/LighthouseSocial.WebApi/Controllers/PhotoController.cs
+++ src/LighthouseSocial.WebApi/Controllers/PhotoController.cs
@@ -4,58 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LighthouseSocial.WebApi.Controllers;
 
-public record UploadPhotoRequest(string FileName, string CameraType, Guid UserId, Guid LighthouseId, string Resolution, string Lens);
-
 [ApiController]
 [Route("api/[controller]")]
 public class PhotoController(ILogger<PhotoController> logger, IPhotoService photoService, IPhotoUploadService photoUploadService)
     : ControllerBase
 {
-    //todo@buraksenyurt S6990 bulgusunu çözmek için Interface bazında controller'ları ayrıştır.
-    [HttpPost("upload")]
-    public async Task<ActionResult<Guid>> UploadAsync([FromForm] UploadPhotoRequest request, IFormFile file)
-    {
-        try
-        {
-            if (file == null || file.Length == 0)
-                return BadRequest("No file uploaded.");
-
-            if (file.Length > 5 * 1024 * 1024)
-                return BadRequest("File size exceeds the 5 MB limit.");
-
-            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
-            if(!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
-                return BadRequest("Invalid file type. Only JPG and PNG are allowed.");
-
-            using var stream = file.OpenReadStream();
-            var dto = new PhotoDto
-            (
-                Guid.NewGuid(),
-                request.FileName,
-                DateTime.UtcNow,
-                request.CameraType,
-                request.UserId,
-                request.LighthouseId,
-                request.Resolution,
-                request.Lens
-            );
-            
-            // var result = await photoService.UploadAsync(dto, stream);
-
-            var result = await photoUploadService.UploadAsync(dto, stream);
-
-            if (!result.Success)
-                return BadRequest(result.ErrorMessage);
-
-            return Ok(result.Data);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Error uploading photo for LighthouseId {LighthouseId}", request.LighthouseId);
-            return StatusCode(500, "An unexpected error occurred. Please try again later.");
-        }
-    }
-
     [HttpGet("{fileName}")]
     public async Task<IActionResult> GetRawPhotoAsync(string fileName)
     {
