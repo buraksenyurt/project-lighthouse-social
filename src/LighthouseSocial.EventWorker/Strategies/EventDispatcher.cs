cab858f9b40613b2a789f57e4d4a258ced5eb1e7
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-11-06 16:55:19 +0300

    added strategy pattern

---
+++ src/LighthouseSocial.EventWorker/Strategies/EventDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using LighthouseSocial.EventWorker.Services;
+
+namespace LighthouseSocial.EventWorker.Strategies;
+
+public class EventDispatcher
+{
+    private readonly Dictionary<string, IEventStrategy> _strategies;
+
+    public EventDispatcher(IEnumerable<IEventStrategy> strategies)
+    {
+        _strategies = strategies.ToDictionary(s => s.EventType, s => s);
+    }
+
+
+    public async Task DispatchAsync(RabbitMqEventConsumerService.EventMessage eventMessage, CancellationToken cancellationToken)
+    {
+        if (_strategies.TryGetValue(eventMessage.EventType, out var strategy))
+        {
+            await strategy.HandleEventAsync(eventMessage, cancellationToken);
+        }
+        //log: no strategy about eventType
+    }
+}
---
+++ src/LighthouseSocial.EventWorker/Strategies/IEventStrategy.cs
@@ -0,0 +1,9 @@
+﻿using LighthouseSocial.EventWorker.Services;
+
+namespace LighthouseSocial.EventWorker.Strategies;
+
+public interface IEventStrategy
+{
+    public string EventType { get; set; }
+    public Task HandleEventAsync(RabbitMqEventConsumerService.EventMessage eventMessage, CancellationToken cancellationToken);
+}
